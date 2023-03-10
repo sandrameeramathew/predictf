7b89c28ee2c0fb64eeb01299fda408862692ca08
sandra meera mathew <126272826+sandrameeramathew@users.noreply.github.com>
2023-03-10 12:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+
+import os
+import pandas as pd
+
+import numpy as np
+import matplotlib.pyplot as plt
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.callbacks import EarlyStopping,ModelCheckpoint
 import streamlit as st
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -20,40 +36,59 @@ def add_bg_from_local(image_file):
     )
 add_bg_from_local('sales.jpg')  
 
-# Load the sales data
-sales_df = pd.read_csv('sales_data.csv')
+store_sales=pd.read_csv("train.csv")
+store_sales= store_sales.drop(['store', 'item'], axis=1)
+store_sales['date'] = pd.to_datetime(store_sales['date'])
+store_sales['date'] = store_sales['date'].dt.to_period("M")
+monthly_sales = store_sales.groupby('date').sum().reset_index()
+monthly_sales['date'] = monthly_sales['date'].dt.to_timestamp()
+monthly_sales['sales_diff'] = monthly_sales['sales'].diff() 
+monthly_sales = monthly_sales.dropna() 
+supervised_data = monthly_sales.drop(['date', 'sales'],axis=1)
+for i in range(1,13):
+  col_name='month' + str(i)
+  supervised_data[col_name] = supervised_data['sales_diff'].shift(i) 
+supervised_data =supervised_data.dropna().reset_index(drop=True)
+train_data = supervised_data[:-12]
+test_data = supervised_data[-12:]
+scaler = MinMaxScaler (feature_range=(-1,1)) 
+scaler.fit(train_data)
+train_data = scaler.transform(train_data)
+test_data = scaler.transform(test_data)
+x_train, y_train= train_data[:,1:],train_data[:,0:1] 
+x_test, y_test =test_data[:,1:], test_data[:,0:1]
+y_train =y_train.ravel() 
+y_test= y_test.ravel()
+sales_dates= monthly_sales['date'][-12:].reset_index(drop=True)
+predict_df = pd.DataFrame(sales_dates)
 
-# Create the feature matrix and target vector
-X = sales_df[['item_name', 'day']]
-y = sales_df['sales']
+act_sales= monthly_sales['sales'][-13:].to_list()
+lr_model= LinearRegression()
+lr_model.fit(x_train, y_train) 
+lr_pre= lr_model.predict(x_test)
+lr_pre =lr_pre.reshape(-1, 1)
+lr_pre_test_set = np.concatenate([lr_pre, x_test], axis=1) 
+lr_pre_test_set = scaler.inverse_transform(lr_pre_test_set)
+result_list =[]
+for index in range(0, len(lr_pre_test_set)):
+  result_list.append(lr_pre_test_set[index][0] + act_sales[index]) 
+lr_pre_series = pd.Series (result_list, name="Linear Prediction")
+predict_df= predict_df.merge(lr_pre_series, left_index= True, right_index=True)
+lr_mse =np.sqrt(mean_squared_error(predict_df['Linear Prediction'], monthly_sales['sales'][-12:]))
 
-# Convert categorical variables to dummy variables
-X = pd.get_dummies(X, columns=['day', 'item_name'], prefix=['day', 'item_name'])
+lr_mae= mean_absolute_error(predict_df['Linear Prediction'], monthly_sales['sales'][-12:])
 
-# Create the linear regression model
-model = LinearRegression()
+lr_r2 =r2_score (predict_df['Linear Prediction'], monthly_sales['sales'][-12:])
 
-# Train the model on the sales data
-model.fit(X, y)
-
-# Calculate the mean squared error and R^2 score for the model
-y_pred = model.predict(X)
-mse = mean_squared_error(y, y_pred)
-r2 = r2_score(y, y_pred)
-print('Mean squared error: ', mse)
-print('R^2 score:', r2)
 
 # Get input from user
-item_name =st.selectbox(
-     'select item name',
-    ('vada', 'samoosa', 'cream bun','pazhampori','bajji'))
-day = st.selectbox(
-    'select day',
-    ('Monday', 'Tuesday', 'Wednesday','Thursday','Friday'))
+date =st.selectbox(
+     'select datename',
+    ('1/2/2019', 'samoosa', 'cream bun','pazhampori','bajji'))
 
 # Create new input
-X_new = pd.DataFrame({'item_name': [item_name], 'day': [day]})
-X_new = pd.get_dummies(X_new, columns=['day', 'item_name'], prefix=['day', 'item_name'])
+X_new = pd.DataFrame('date': [date])
+X_new = pd.get_dummies(X_new, columns=['date'], prefix=['date'])
 
 # Add missing dummy variables
 missing_cols = set(X.columns) - set(X_new.columns)
@@ -64,7 +99,7 @@ for col in missing_cols:
 X_new = X_new[X.columns]
 
 # Predict the sales for the new input
-y_new = model.predict(X_new)
+y_new = lr_model.predict(X_new)
 if st.button('Predict'):
     st.write('Predicted sales: ', y_new[0])
    
