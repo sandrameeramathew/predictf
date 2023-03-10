b9f4d0fc8f155162d4215cd4e181764108f6f1ef
sandra meera mathew <126272826+sandrameeramathew@users.noreply.github.com>
2023-03-10 21:39:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 import numpy as np
 import matplotlib.pyplot as plt
-from xgboost import XGBRegressor
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.tree import DecisionTreeRegressor
-from xgboost import XGBRegressor
+
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
